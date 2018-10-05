2fd526fb5cb94cf7fa4179a0d78b74959fa4333b
nathanael.lemuella <nathanael.lemuella@ui.ac.id>
2018-10-05 18:20:34 +0700

    dead

---
+++ tutorial-4/src/main/java/com/apap/tutorial4/repository/FlightDb.java
@@ -0,0 +1,18 @@
+package com.apap.tutorial4.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.apap.tutorial4.model.FlightModel;
+
+/**
+ * FlightDB
+ * 
+ * @author Nathanael
+ *
+ */
+
+@Repository
+public interface FlightDb extends JpaRepository<FlightModel, Long> {
+	FlightModel findFlightById(long id);
+}
